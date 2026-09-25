e4a382a67d27239e3eefd6e39b22b43d61423b86
Luke Roberts <luke@mewt.eu>
2026-09-25 16:16:06 +0100

    Implement Lambda handler to call exif cleaner method

---
+++ src/handler.py
@@ -0,0 +1,39 @@
+import json
+import os
+import urllib.parse
+import boto3
+from exif_cleaner import clean_exif_data
+
+s3_client = boto3.client('s3')
+
+destination_bucket = os.environ['DESTINATION_BUCKET']
+
+def handler(event, context):
+    records = event.get('Records', [])
+    print('Processing', len(records), 'records.')
+
+    for record in records:
+        source_bucket = record['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+
+        print('Reading image with key', key, 'from bucket', source_bucket)
+
+        # Fetch image from S3
+        response = s3_client.get_object(Bucket=source_bucket, Key=key)
+        raw_image = response['Body']
+
+        # Clean EXIF data from image
+        cleaned_image = clean_exif_data(raw_image)
+
+        # Save cleaned image back to destination S3 bucket
+        s3_client.put_object(
+            Bucket=destination_bucket,
+            Key=key,
+            Body=cleaned_image,
+            ContentType=response.get('ContentType', 'image/jpeg')
+        )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully cleaned EXIF data from image(s).')
+    }
