ad022cf221e666d8171e5abcebe8de6c8f99323c
Luke Roberts <luke@mewt.eu>
2026-09-25 18:43:47 +0100

    Remove unneeded file used for testing
    As we now have actual tests

--- src/__main__.py
+++
@@ -1,8 +0,0 @@
-from exif_cleaner import clean_exif_data
-
-if __name__ == '__main__':
-    # Clean the image
-    output = clean_exif_data('photo.jpg')
-
-    # Save to disk for easy testing
-    output.save('clean_photo.jpg')
